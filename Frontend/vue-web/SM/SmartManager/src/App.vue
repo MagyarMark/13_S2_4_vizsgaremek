a3d9ba22f1ab20dc1eed5d5b29e514c68326bd1c
MagyarMark <118881797+MagyarMark@users.noreply.github.com>
2025-10-15 19:51:04 +0200

    Node.js + Express

--- Frontend/vue-web/SM/SmartManager/src/App.vue
+++ Frontend/vue-web/SM/SmartManager/src/App.vue
@@ -9,3 +9,27 @@
 <style>
 
 </style>
+
+<!--<template>
+  <div>
+    <h2>Backend kapcsolat teszt</h2>
+    <button @click="fetchData">Kapcsolat tesztelése</button>
+    <p v-if="message">{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return { message: "" };
+  },
+  methods: {
+    async fetchData() {
+      const res = await fetch("http://localhost:3000/api/test");
+      const data = await res.json();
+      this.message = `Kapcsolat rendben! Idő: ${data.time.now}`;
+    }
+  }
+};
+</script>
+-->
